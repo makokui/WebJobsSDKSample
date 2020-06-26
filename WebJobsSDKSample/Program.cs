676b79e72a3cbe5f6ac250ec4a236610e5b3cb7e
Masafumi Kokui <makokui@microsoft.com>
2020-06-26 19:59:25 +0900

    プロジェクト ファイルを追加します。

---
+++ WebJobsSDKSample/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Npgsql;
+
+namespace WebJobsSDKSample
+{
+    class Program
+    {
+        private static string Host = "{host name}";
+        private static string User = "{user name}";
+        private static string DBname = "{db name}";
+        private static string Password = "{password}";
+        private static string Port = "5432";
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            // Build connection string using parameters from portal
+            //
+            string connString =
+                String.Format(
+                    "Server={0};Username={1};Database={2};Port={3};Password={4};SSLMode=Prefer",
+                    Host,
+                    User,
+                    DBname,
+                    Port,
+                    Password);
+
+
+            using (var conn = new NpgsqlConnection(connString))
+
+            {
+                Console.Out.WriteLine("Opening connection");
+                conn.Open();
+
+                using (var command = new NpgsqlCommand("DROP TABLE IF EXISTS inventory", conn))
+                {
+                    command.ExecuteNonQuery();
+                    Console.Out.WriteLine("Finished dropping table (if existed)");
+                }
+
+                using (var command = new NpgsqlCommand("CREATE TABLE inventory(id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER)", conn))
+                {
+                    command.ExecuteNonQuery();
+                    Console.Out.WriteLine("Finished creating table");
+                }
+
+                using (var command = new NpgsqlCommand("INSERT INTO inventory (name, quantity) VALUES (@n1, @q1), (@n2, @q2), (@n3, @q3)", conn))
+                {
+                    command.Parameters.AddWithValue("n1", "banana");
+                    command.Parameters.AddWithValue("q1", 150);
+                    command.Parameters.AddWithValue("n2", "orange");
+                    command.Parameters.AddWithValue("q2", 154);
+                    command.Parameters.AddWithValue("n3", "apple");
+                    command.Parameters.AddWithValue("q3", 100);
+
+                    int nRows = command.ExecuteNonQuery();
+                    Console.Out.WriteLine(String.Format("Number of rows inserted={0}", nRows));
+                }
+            }
+
+            Console.WriteLine("Fin.");
+            //Console.ReadLine();
+        }
+    }
+}
